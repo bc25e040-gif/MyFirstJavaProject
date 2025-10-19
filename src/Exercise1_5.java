052e9b2e9e6c87f7f9480d6ed3b94480905a5dc3
NW E <ww@NWEMacBook-Air.local>
2025-10-19 23:22:25 +0800

    My first Java exercises submission

---
+++ src/Exercise1_5.java
@@ -0,0 +1,6 @@
+public class Exercise1_5 {
+    public static void main(String[] args) {
+        double result = (7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5);
+        System.out.println("The result of the expression is: " + 0.8392857142857143);
+    }
+}
---
+++ src/MyProfile.java
@@ -0,0 +1,12 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        System.out.println("*******************************");
+        System.out.println("*         My Profile          *");
+        System.out.println("*******************************");
+        System.out.println("Name: Zeng Jingwen");          // 
+        System.out.println("Matric: BC25E040");           // 
+        System.out.println("State/Province: Guangdong");  // 
+        System.out.println("Is this program your first choice?: Yes"); // 
+        System.out.println("Expectations from this course: Learn programming basics."); // 
+    }
+}
