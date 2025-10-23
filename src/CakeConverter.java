db64388f05434aa9495d1bf072b097e06a3bcf52
NW E <ww@NWEMacBook-Air.local>
2025-10-23 21:28:56 +0800

    Initial commit

--- src/CakeConverter.java
+++ src/CakeConverter.java
@@ -1,9 +1,12 @@
 
 public class CakeConverter {
+    public static void main(String[] args) {
+        double grams = 500;
+        double kilograms = grams / 1000;
+        double pounds = kilograms * 2.20462;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        System.out.println("Grams: " + grams);
+        System.out.println("Kilograms: " + kilograms);
+        System.out.println("Pounds: " + pounds);
+    }
 }
